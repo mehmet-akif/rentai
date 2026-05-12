ce554d7391dd068fe9ef9dbe1ba5769dfb486b7b
mehmet-akif <makif8992@gmail.com>
2026-05-12 16:06:33 -0400

    Connect frontend to deployed backend

--- frontend/src/api/listingsApi.js
+++ frontend/src/api/listingsApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5001/api";
+const API_BASE_URL = "https://rentai-1392.onrender.com/api";
 
 export const getListings = async (params = {}) => {
   const response = await axios.get(`${API_BASE_URL}/listings`, { params });
