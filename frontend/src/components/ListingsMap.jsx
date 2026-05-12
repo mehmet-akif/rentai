51bd0a28ab5baecd4c77162049b9b59d6c3e553a
mehmet-akif <makif8992@gmail.com>
2026-05-12 15:29:17 -0400

    Fix Leaflet marker icons for deployment

--- frontend/src/components/ListingsMap.jsx
+++ frontend/src/components/ListingsMap.jsx
@@ -1,6 +1,20 @@
 import { useEffect, useRef, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+
 import "leaflet/dist/leaflet.css";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow
+});
 
 function ListingsMap({ listings }) {
   const [isFullscreen, setIsFullscreen] = useState(false);
